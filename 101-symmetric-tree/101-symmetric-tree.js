fb8168f3f46fc2e26c23d391163207e5e9bf965c
Bekim Sinanovic <86640828+beksina@users.noreply.github.com>
2022-07-19 22:37:40 -0400

    Time: 89 ms (69.59%), Space: 44.4 MB (75.74%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.js
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -11,6 +11,34 @@
  * @return {boolean}
  */
 
+// BFS
+function isSymmetric(root) {
+    const lQ = [root.left];
+    const rQ = [root.right];
+    
+    while(lQ.length && rQ.length) {
+        let rightNode = rQ.shift();
+        let leftNode = lQ.shift();
+        
+        
+        if (rightNode === null && leftNode === null) {
+            continue;
+        } else if (rightNode === null && leftNode !== null || rightNode !== null && leftNode === null || rightNode.val !== leftNode.val) {
+            return false;
+        }
+        
+        rQ.push(rightNode.right);
+        rQ.push(rightNode.left);
+        lQ.push(leftNode.left);
+        lQ.push(leftNode.right);
+    }
+    return lQ.length === 0 && rQ.length === 0;
+}
+
+
+/*
+DFS
+
 function isSymmetric(root) {
     if (root.left === null && root.right === null) return true;
     let leftRoot = root.left;
@@ -26,3 +54,4 @@ function isSymmetric(root) {
     
     return traverse(rightRoot, leftRoot);
 };
+*/
