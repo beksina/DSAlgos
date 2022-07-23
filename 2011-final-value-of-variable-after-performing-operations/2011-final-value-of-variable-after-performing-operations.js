7e6528f421d8675efbbbab411ab980cb77fe42d5
Bekim Sinanovic <86640828+beksina@users.noreply.github.com>
2022-07-22 23:01:50 -0400

    Time: 66 ms (93.53%), Space: 42.7 MB (43.86%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -3,8 +3,15 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    const ops = {"+": 1, "-": -1};
-    return operations.reduce((x, curr) => {
-        return x + ops[curr[1]];
-    }, 0)
+    // const ops = {"+": 1, "-": -1};
+    // return operations.reduce((x, curr) => {
+    //     return x + ops[curr[1]];
+    // }, 0)
+    
+    let x = 0;
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i][1] === "+") x++
+        else x--;
+    }
+    return x;
 };
